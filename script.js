207cae6734f7eccd103a2402a2ce018eaa3b180c
Elia <phoe.9518@gmail.com>
2026-06-01 20:27:24 +0200

    js mod

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const btn = document.getElementById('btn');
 
-btn.addEventListener('click', function() {
-    console.log('Button clicked!');
+btn.addEventListener('click', function(event) {
+    event.preventDefault(); 
+    
+    console.log("Bottone cliccato senza ricaricare!");
 });
